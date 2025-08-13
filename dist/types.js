b2c2267d14223a5855577b6e34cdcea0b5364351
Mario Chaveev <92914733+harkaca@users.noreply.github.com>
2025-08-13 06:38:35 +0300

    Update types.js

--- dist/types.js
+++ dist/types.js
@@ -1,1 +1,13 @@
-export {};
+export type SymbolID = number;
+
+export interface SpinResult {
+  screen: SymbolID[][];
+  lineResults: {
+    lineIndex: number;
+    symbol: SymbolID;
+    count: number;
+    payout: number;
+  }[];
+  totalPayout: number;
+  
+}
